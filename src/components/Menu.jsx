b7bba04feba6d9e6d26453e88ab7598515efefa2
Daniel Lozano <59983507+DanielLoRo31@users.noreply.github.com>
2020-10-11 15:34:42 -0500

    feat: fixed scroll menu buttons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,91 +3,180 @@ import ApiService from "../services/ApiService";
 
 import { Container, Row, Col } from "react-bootstrap";
 import { withRouter } from "react-router-dom";
+import { Waypoint } from "react-waypoint";
+import { Link } from "react-scroll";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/menu.css";
-import Tacos from "../img/tacos.jpg";
 
 class Menu extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      menu: '',
+      menu: "",
       restaurantName: props.match.params.id,
-      categories: ''
+      categories: "",
+      styleButtons: [],
+      fixedMenu: ["", "visibility-hidden"],
     };
   }
   async componentDidMount() {
     document.title = this.state.restaurantName;
 
-    console.log(this.state.restaurantName)
+    console.log(this.state.restaurantName);
 
-    var response = await ApiService.getById(this.state.restaurantName, 'menus')
+    var response = await ApiService.getById(this.state.restaurantName, "menus");
 
-    var categories = await ApiService.getById(this.state.restaurantName, 'menus/categories')
+    var categories = await ApiService.getById(
+      this.state.restaurantName,
+      "menus/categories"
+    );
 
-    if (response) {
+    if (response && categories) {
+      var array = [];
+      for (let i = 0; i < categories.data.length; i++) {
+        i === 0
+          ? array.push("menu-category-active-btn")
+          : array.push("menu-category-btn");
+      }
+
       this.setState({
         ...this.state,
         menu: response.data,
-        categories: categories.data
-      })
-    }
+        categories: categories.data,
+        styleButtons: array,
+      });
 
+      this.orderCategoriesDishes();
+    }
+    window.addEventListener("scroll", this.handleScroll.bind(this));
   }
 
-  renderLoadedDishes() {
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll.bind(this));
+  }
 
-    if (this.state.menu.dishes) {
-      return this.state.menu.dishes.map((f) => (
-        <Col xs="12">
-          <div className="food-container">
-            <div className="food-texto">
-              <h1>{f.name}</h1>
-              <h2>{f.description}</h2>
-            </div>
-            <div className="food-img" style={{ backgroundImage: `url(${f.img})` }}></div>
-            <div className="food-button">
-              ${f.price}
-            </div>
-          </div>
-        </Col>
-      ));
+  handleScroll(event) {
+    var top = event.target.scrollingElement.scrollTop;
+    if (top > 255) {
+      this.setState({
+        ...this.state,
+        fixedMenu: ["div-fixed-menu", "visibility-visible"],
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        fixedMenu: ["", "visibility-hidden"],
+      });
     }
-    else {
-      return
+  }
+
+  _changeButtonStyle(index) {
+    var array = [];
+    for (let i = 0; i < this.state.styleButtons.length; i++) {
+      i === index
+        ? array.push("menu-category-active-btn")
+        : array.push("menu-category-btn");
     }
+    this.setState({
+      ...this.state,
+      styleButtons: array,
+    });
   }
 
   renderCategories() {
     if (this.state.categories) {
       return this.state.categories.map((f, index) => (
-        <button key={index}>{f}</button>
-      ))
+        <Link
+          onClick={this._changeButtonStyle.bind(this, index)}
+          className={this.state.styleButtons[index]}
+          key={index}
+          to={`row${index}`}
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          {f}
+        </Link>
+      ));
     }
 
-    return
-
+    return;
   }
 
+  renderLoadedDishes(dishes) {
+    return dishes.map((f) => (
+      <Col xs="12">
+        <div className="food-container">
+          <div className="food-texto">
+            <h1>{f.name}</h1>
+            <h2>{f.description}</h2>
+          </div>
+          <div
+            className="food-img"
+            style={{ backgroundImage: `url(${f.img})` }}
+          ></div>
+          <div className="food-button">${f.price}</div>
+        </div>
+      </Col>
+    ));
+  }
+
+  renderWaypoints() {
+    if (this.state.menu.dishes) {
+      var dishes = this.orderCategoriesDishes();
+      return dishes.map((d, index) => (
+        <Waypoint
+          onEnter={this._changeButtonStyle.bind(this, index)}
+          onLeave={this._changeButtonStyle.bind(this, index - 1)}
+        >
+          <Row id={`row${index}`}>{this.renderLoadedDishes(d)}</Row>
+        </Waypoint>
+      ));
+    }
+
+    return;
+  }
+
+  orderCategoriesDishes() {
+    if (this.state.categories) {
+      var array = [];
+      this.state.categories.map((c, index) => {
+        var secondArray = [];
+        this.state.menu.dishes.map((m) => {
+          if (c.includes(m.category)) {
+            secondArray.push(m);
+          }
+        });
+        array.push(secondArray);
+      });
+      return array;
+    }
+    return;
+  }
 
   render() {
     return (
-      <div className="mt-3">
+      <div>
+        <div className="div-fixed-menu"></div>
         <div
           className="div-menu-img"
           style={{ backgroundImage: `url(${this.state.menu.bannerImg})` }}
         >
           <div className="logo-circle">
-            <div style={{ backgroundImage: `url(${this.state.menu.logoImg})` }}></div>
+            <div
+              style={{ backgroundImage: `url(${this.state.menu.logoImg})` }}
+            ></div>
           </div>
         </div>
         <div className="logo-space"></div>
-        <div className="opciones-button">
+
+        <div className={`opciones-button ${this.state.fixedMenu[0]}`}>
           {this.renderCategories()}
         </div>
-        <Container>
-          <Row>{this.renderLoadedDishes()}</Row>
-        </Container>
+        <div className={`div-invisible ${this.state.fixedMenu[1]}`}></div>
+        <Container>{this.renderWaypoints()}</Container>
       </div>
     );
   }
--- src/components/home-components/QrTest.jsx
+++ src/components/home-components/QrTest.jsx
@@ -1,41 +1,39 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import {
-  ListGroup,
-  Container,
-  Row,
-  Col,
-  Image
-} from 'react-bootstrap';
-import '../../css/generalStyles.css'
-import qrImage from '../../img/qrtest.png'
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { ListGroup, Container, Row, Col, Image } from "react-bootstrap";
+import "../../css/generalStyles.css";
+import qrImage from "../../img/qrtest.png";
+import { Waypoint } from "react-waypoint";
 
 class QrTest extends React.Component {
+  _hola() {
+    console.log("hola");
+  }
+  _adios() {
+    console.log("adios");
+  }
 
   render() {
     return (
-
-      <div>
-
+      <Waypoint
+        onEnter={this._hola.bind(this)}
+      >
         <Container id="section3">
           <Row clasName="p-5 mt-5">
             <Col sm={4}>
               <img src={qrImage} alt="qrImage" />
             </Col>
             <Col sm={8}>
-              <h1 className="mt-5 qrTitle">
-                Haz la prueba
-              </h1>
+              <h1 className="mt-5 qrTitle">Haz la prueba</h1>
               <h3>
-                ad minim veniam, quis nostrud
-                exerci tation ullamcorper suscipit
+                ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
                 lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel
                 eum iriure dolor in hendrerit in vul
               </h3>
             </Col>
           </Row>
         </Container>
-      </div>
+      </Waypoint>
     );
   }
 }
